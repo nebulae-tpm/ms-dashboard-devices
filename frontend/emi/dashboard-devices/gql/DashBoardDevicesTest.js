26917acd45199e93f0205eea4027af800b2da454
juansmolano <juansmolano@gmail.com>
2018-04-12 19:17:31 -0500

    Fixed angular query test

--- frontend/emi/dashboard-devices/gql/DashBoardDevicesTest.js
+++ frontend/emi/dashboard-devices/gql/DashBoardDevicesTest.js
@@ -3,8 +3,8 @@ import gql from 'graphql-tag';
 // We use the gql tag to parse our query string into a query document
 export const DashBoardDevicesTest = gql`
     query{
-        DashBoardDevicesTest(firstName:""){
-            id
+        getDashBoardDevicesAlarmReport(type:RAM_MEMORY){
+            type
         }
     }
 `;
